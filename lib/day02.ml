fbd484db2306595b0a4eacb278233d8a59ec1756
Michael Da Costa <mdjcosta30@gmail.com>
2024-12-02 11:05:52 +0200

    feat: day 02, part 01 working solution

--- lib/day02.ml
+++ lib/day02.ml
@@ -5,11 +5,22 @@ let rec in_bound bound = function
     if check > bound || check <= 0 then false else in_bound bound rest
 ;;
 
+let rec ascending = function
+  | [] | [ _ ] -> true
+  | x :: (y :: _ as rest) -> if x < y then ascending rest else false
+;;
+
+let rec descending = function
+  | [] | [ _ ] -> true
+  | x :: (y :: _ as rest) -> if x > y then descending rest else false
+;;
+
 let part01 filename =
   let total =
     Helpers.File.read_file filename
     |> List.map (String.split_on_char ' ')
     |> List.map (List.map int_of_string)
+    |> List.filter (fun x -> ascending x || descending x)
     |> List.map (in_bound 3)
     |> List.fold_left (fun count x -> if x then count + 1 else count) 0
   in
--- lib/helpers/lists.ml
+++ lib/helpers/lists.ml
@@ -7,3 +7,11 @@ let count_occurances input_list =
       | None -> 1
       | Some count -> count + 1))
 ;;
+
+let rec get_at_index lst idx =
+  match lst, idx with
+  | [], _ -> failwith "Index out of bounds"
+  | x :: _, 0 -> x
+  | _ :: xs, n when n > 0 -> get_at_index xs (n - 1)
+  | _, _ -> failwith "Invalid index"
+;;
