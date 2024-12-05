0a42d2f88047fc9de3090ce91f36e2f7df755db6
Michael Da Costa <mdjcosta30@gmail.com>
2024-12-05 10:40:28 +0200

    WIP: prepending to the list, as it means I don't have to iterate each time to add a word

--- lib/day04.ml
+++ lib/day04.ml
@@ -4,14 +4,14 @@ let part01_match_func grid i j =
   let result = ref [] in
   let word_length = 4 in
   let l = word_length - 1 in
-  result := !result @ [ get_up grid i j l ];
-  result := !result @ [ get_up_right grid i j l ];
-  result := !result @ [ get_right grid i j l ];
-  result := !result @ [ get_down_right grid i j l ];
-  result := !result @ [ get_down grid i j l ];
-  result := !result @ [ get_down_left grid i j l ];
-  result := !result @ [ get_left grid i j l ];
-  result := !result @ [ get_up_left grid i j l ];
+  result := get_up grid i j l :: !result;
+  result := get_up_right grid i j l :: !result;
+  result := get_right grid i j l :: !result;
+  result := get_down_right grid i j l :: !result;
+  result := get_down grid i j l :: !result;
+  result := get_down_left grid i j l :: !result;
+  result := get_left grid i j l :: !result;
+  result := get_up_left grid i j l :: !result;
   !result |> List.filter (fun word -> word = "XMAS") |> List.length
 ;;
 
