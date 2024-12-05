4b42e738797b45ce342ffff09a8ab15df2fa1e5b
Michael Da Costa <mdjcosta30@gmail.com>
2024-12-05 11:18:18 +0200

    feat: day 04 both parts, and some formatting changes

--- lib/day04.ml
+++ lib/day04.ml
@@ -21,3 +21,27 @@ let part01 filename =
   let total = walk_grid grid 'X' part01_match_func in
   total
 ;;
+
+let valid_pairs p1 p2 =
+  match p1 with
+  | "AM", "AS" | "AS", "AM" ->
+    (match p2 with "AM", "AS" -> true | "AS", "AM" -> true | _ -> false)
+  | _ -> false
+;;
+
+let part02_match_func grid i j =
+  let top_right = get_up_right grid i j 1 in
+  let bottom_left = get_down_left grid i j 1 in
+  let p1 = top_right, bottom_left in
+  let top_left = get_up_left grid i j 1 in
+  let bottom_right = get_down_right grid i j 1 in
+  let p2 = top_left, bottom_right in
+  if valid_pairs p1 p2 then 1 else 0
+;;
+
+let part02 filename =
+  let data = Helpers.File.read_file filename in
+  let grid = make_grid data in
+  let total = walk_grid grid 'A' part02_match_func in
+  total
+;;
