4963646bc40d1b610061d0313b061cdec1cc4c28
Michael Da Costa <mdjcosta30@gmail.com>
2024-12-05 11:21:31 +0200

    chore: semantics'

--- bin/main.ml
+++ bin/main.ml
@@ -3,43 +3,43 @@ let () = print_endline "AOC 2024, in OCaml"
 (* Day 01 *)
 let () =
   let total = Aoc2024.Day01.part01 "./data/day01.txt" in
-  Printf.printf "Day 01, Part 01, Total: %d\n" total
+  Printf.printf "Day 01, Part 01, answer: %d\n" total
 ;;
 
 let () =
   let total = Aoc2024.Day01.part02 "./data/day01.txt" in
-  Printf.printf "Day 01, Part 02, Total: %d\n" total
+  Printf.printf "Day 01, Part 02, answer: %d\n" total
 ;;
 
 (* Day 02 *)
 let () =
   let total = Aoc2024.Day02.part01 "./data/day02.txt" in
-  Printf.printf "Day 02, Part 01, Total: %d\n" total
+  Printf.printf "Day 02, Part 01, answer: %d\n" total
 ;;
 
 let () =
   let total = Aoc2024.Day02.part02 "./data/day02.txt" in
-  Printf.printf "Day 02, Part 02, Total: %d\n" total
+  Printf.printf "Day 02, Part 02, answer: %d\n" total
 ;;
 
 (* Day 03 *)
 let () =
   let total = Aoc2024.Day03.part01 "./data/day03.txt" in
-  Printf.printf "Day 03, Part 01, Total: %d\n" total
+  Printf.printf "Day 03, Part 01, answer: %d\n" total
 ;;
 
 let () =
   let total = Aoc2024.Day03.part02 "./data/day03.txt" in
-  Printf.printf "Day 03, Part 02, Total: %d\n" total
+  Printf.printf "Day 03, Part 02, answer: %d\n" total
 ;;
 
 (* Day 04 *)
 let () =
   let total = Aoc2024.Day04.part01 "./data/day04.txt" in
-  Printf.printf "Day 04, Part 01, Total: %d\n" total
+  Printf.printf "Day 04, Part 01, answer: %d\n" total
 ;;
 
 let () =
   let total = Aoc2024.Day04.part02 "./data/day04.txt" in
-  Printf.printf "Day 04, Part 02, Total: %d\n" total
+  Printf.printf "Day 04, Part 02, answer: %d\n" total
 ;;
